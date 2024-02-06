683728c6bffd7bcfbc9909dbc34ff18b4ba916ae
salmanlearning <106384673+salmanlearning@users.noreply.github.com>
2024-02-06 12:02:34 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -70,19 +70,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Salman Ibrahim, you can call me Salman");
         System.out.println();
 
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("Become a full-stack developer and get a job");
+        System.out.println("Be able to work remotely abroad");
         System.out.println();
 
         System.out.println("My hobbies");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("Skiing");
+        System.out.println("Soccer");
+        System.out.println("Gaming");
+        System.out.println("Gym");
 
         System.out.println();
     }
